542b11b7c284084e87a15eddcd63f746caad1f4d
Asami Kawamoto <asami236@gmail.com>
2015-12-10 23:12:15 -0800

    Add total to the cart

--- source/js/controllers/cart.js
+++ source/js/controllers/cart.js
@@ -3,7 +3,10 @@
 var app = angular.module('paymentApp');
 
 app.controller('cartCtrl', function($scope, $state, BookService) {
-  $scope.cart = $scope.$storage.cart  
+  $scope.cart = $scope.$storage.cart;  
 
+  $scope.total = $scope.cart.reduce(function(sum, book){
+    return sum + book.price;
+  },0)
 
 });
