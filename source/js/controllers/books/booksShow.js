5645bdc5d2da1d612b690dd30bd4b74e5730490c
Asami Kawamoto <asami236@gmail.com>
2015-12-10 23:38:11 -0800

    Add checkout button

--- source/js/controllers/books/booksShow.js
+++ source/js/controllers/books/booksShow.js
@@ -37,20 +37,4 @@ app.controller('booksShowCtrl', function($scope, $state, $http, ENV, BookService
 
   // }
 
-  $scope.doCheckout = function(tokenObj) {
-    $http.post(`${ENV.API_URL}/checkout`, {
-      tokenObj: tokenObj,
-      book: $scope.book
-    })
-    .then(function(res) {
-      console.log('res:', res);
-    }, function(err) {
-      console.log('err:', err);
-    })
-  };
-
-  $scope.formatPrice = function(num) {
-    return Math.round(num * 100);
-  };
 });
-
--- source/js/controllers/cart.js
+++ source/js/controllers/cart.js
@@ -2,11 +2,27 @@
 
 var app = angular.module('paymentApp');
 
-app.controller('cartCtrl', function($scope, $state, BookService) {
+app.controller('cartCtrl', function($scope, $state, BookService, $http, ENV) {
   $scope.cart = $scope.$storage.cart;  
 
   $scope.total = $scope.cart.reduce(function(sum, book){
     return sum + book.price;
   },0)
+
+  $scope.doCheckout = function(tokenObj) {
+    $http.post(`${ENV.API_URL}/checkout`, {
+      tokenObj: tokenObj,
+      book: $scope.book
+    })z1
+    .then(function(res) {
+      console.log('res:', res);
+    }, function(err) {
+      console.log('err:', err);
+    })
+  };
+
+  $scope.formatPrice = function(num) {
+    return Math.round(num * 100);
+  };
 
 });
